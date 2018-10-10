69d430126a6e2652531753fb3939c97ce39ed0c5
Micah Caffey <mrcaffey@gmail.com>
2018-10-10 10:19:49 -0600

    iniitial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Tracker from './Tracker';
+
+const App = () => (
+  <Tracker />
+)
+
+
+
+
+
+
+export default App
---
+++ src/Tracker.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Tracker extends React.Component {
+  state = { x: 0, y: 0 }
+
+  componentDidMount() {
+    document.addEventListener('mousemove', this.move)
+  }
+
+  move = ({ clientX, clientY }) => {
+    this.setState({ x: clientX, y: clientY })
+  }
+
+  render() {
+    const { x, y } = this.state
+    return (
+      <div>
+        { this.props.children(this.state) }
+      </div>
+    )
+  }
+} 
+
+
+export default Tracker
